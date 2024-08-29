4f7c5b4d08856c4b9ec8b3249fe30aaa21217a06
Vitaliy K. <begemot39@mail.ru>
2024-08-29 13:48:08 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/HW5/calcrestpack/Main.java
@@ -0,0 +1,10 @@
+package ru.netology.javaqa.HW5.calcrestpack;
+
+
+public class Main {
+    public static void main( String[] args ) {
+        CalculationRest calcParty = new CalculationRest();
+
+        System.out.println( calcParty.calcRest( 10_000, 3_000 ) );
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/HW5/calcrestpack/CalculationRestTest.java
@@ -0,0 +1,19 @@
+package ru.netology.javaqa.HW5.calcrestpack;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.api.Assertions;
+
+
+public class CalculationRestTest {
+
+    @ParameterizedTest
+    @CsvFileSource( files="src/test/resources/calcRestParam.csv" )
+    public void calcRestTest( int expected, int income, int expenses ) {
+        CalculationRest restCalc = new CalculationRest();
+
+        int actual = restCalc.calcRest( income, expenses );
+
+        Assertions.assertEquals( expected, actual );
+    }
+}
